b973394a68d3141cb5d61af9a9f140552cf1972d
Mallone <mallone.nogueira@exactaworks.com.br>
2020-09-12 14:51:17 -0300

    feat: update react and remove external lib

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,20 @@ const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
 const WebpackGitHash = require("webpack-git-hash");
 
+function removeExternalReact(defaultConfig) {
+  defaultConfig.externals = defaultConfig.externals.filter(
+    (external) => !RegExp("react").test(external)
+  );
+}
+
 module.exports = (webpackConfigEnv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "single-spa-books",
     projectName: "navbar",
     webpackConfigEnv,
   });
+
+  removeExternalReact(defaultConfig);
 
   return webpackMerge.smart(defaultConfig, {
     output: {
